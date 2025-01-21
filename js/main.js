b51a4eea0f399dd2b9fa05b2aad263f3633cbf1f
violet8969 <155793460+violet8969@users.noreply.github.com>
2025-01-21 13:12:23 -0500

    created playGame function and declared playRound inside

--- js/main.js
+++ js/main.js
@@ -51,42 +51,92 @@ let humanScore = 0
 let computerScore = 0
 
 
-function playRound (humanFinalAnswer, computerFinalAnswer){
-    if (humanFinalAnswer === computerFinalAnswer){
-        console.log("Draw") 
-    }
+// function playRound (humanFinalAnswer, computerFinalAnswer){
+//     if (humanFinalAnswer === computerFinalAnswer){
+//         console.log("Draw") 
+//     }
 
-    else if (humanFinalAnswer === "rock" && computerFinalAnswer === "scissors"){
-        console.log("You win! Rock beats scissors");
-        return humanScore++;
-    }
-    else if (humanFinalAnswer === "rock" && computerFinalAnswer === "paper") {
-        console.log("You Lose! Paper beats rock")
-        return computerScore++;
-    }
-    else if (humanFinalAnswer === "paper" && computerFinalAnswer === "rock"){
-        console.log("You Win! Paper beats rock")
-        return humanScore++;
-    }
+//     else if (humanFinalAnswer === "rock" && computerFinalAnswer === "scissors"){
+//         console.log("You win! Rock beats scissors");
+//         return humanScore++;
+//     }
+//     else if (humanFinalAnswer === "rock" && computerFinalAnswer === "paper") {
+//         console.log("You Lose! Paper beats rock")
+//         return computerScore++;
+//     }
+//     else if (humanFinalAnswer === "paper" && computerFinalAnswer === "rock"){
+//         console.log("You Win! Paper beats rock")
+//         return humanScore++;
+//     }
 
-    else if (humanFinalAnswer === "paper" && computerFinalAnswer === "scissors"){
-        console.log("You Lose! Scissors beat paper")
-        return computerScore++;
-    }
+//     else if (humanFinalAnswer === "paper" && computerFinalAnswer === "scissors"){
+//         console.log("You Lose! Scissors beat paper")
+//         return computerScore++;
+//     }
 
-    else if (humanFinalAnswer === "scissors" && computerFinalAnswer === "paper"){
-        console.log("You Win! Scissors beat paper")
-        return humanScore++;
-    }
+//     else if (humanFinalAnswer === "scissors" && computerFinalAnswer === "paper"){
+//         console.log("You Win! Scissors beat paper")
+//         return humanScore++;
+//     }
 
-    else {
-        console.log("You Lose! Rock beats scissors")
-        return computerScore++;
-    }
+//     else {
+//         console.log("You Lose! Rock beats scissors")
+//         return computerScore++;
+//     }
         
 
-}
+// }
 
-playRound(humanFinalAnswer, computerFinalAnswer)
+// playRound(humanFinalAnswer, computerFinalAnswer){}
+
+
+function playGame(){
+    for (i = 0; i < 5; i++){
+    playRound(humanFinalAnswer, computerFinalAnswer)}
+    function playRound (humanFinalAnswer, computerFinalAnswer){
+        if (humanFinalAnswer === computerFinalAnswer){
+            console.log("Draw") 
+        }
+    
+        else if (humanFinalAnswer === "rock" && computerFinalAnswer === "scissors"){
+            console.log("You win! Rock beats scissors");
+            return humanScore++;
+        }
+        else if (humanFinalAnswer === "rock" && computerFinalAnswer === "paper") {
+            console.log("You Lose! Paper beats rock")
+            return computerScore++;
+        }
+        else if (humanFinalAnswer === "paper" && computerFinalAnswer === "rock"){
+            console.log("You Win! Paper beats rock")
+            return humanScore++;
+        }
+    
+        else if (humanFinalAnswer === "paper" && computerFinalAnswer === "scissors"){
+            console.log("You Lose! Scissors beat paper")
+            return computerScore++;
+        }
+    
+        else if (humanFinalAnswer === "scissors" && computerFinalAnswer === "paper"){
+            console.log("You Win! Scissors beat paper")
+            return humanScore++;
+        }
+    
+        else {
+            console.log("You Lose! Rock beats scissors")
+            return computerScore++;
+        }   
+    
+    }
+     
+}
+ playGame()
+// function playEntireGame(){
+//     for (i = 0; i < 5; i++){
+//         playGame()
+//         return getComputerChoice(), getHumanChoice();
+//     }
+// }
+
+// playEntireGame()
 
 console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`)
