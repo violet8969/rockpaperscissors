29ed2e06d4fc5e3ce4873dc0e886412c1bb15aca
violet8969 <155793460+violet8969@users.noreply.github.com>
2025-01-21 15:04:35 -0500

    cleaned up comments. changed h1 in html

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 
 
 //get choice from computer
-//create function to generate computer choice
-//store computer choice in a variable
+//create function to generate computer choice to string
 // get choice from user
-// allow user input with prompt
-//store user choice in variable
-// convert user choice to all lowercase
-// compare choices
-// if the same output "Draw"
-// if computer wins output "Computer wins"
-//if user wins output " You win"
+// create function for human choice from prompt and convert to lowercase string 
+
+//create playRound function (kept at bottom to show process)
+// compare choices in playRound using logical operators (&&)
+//use conditional statements (if, else) to return score
+//console log the results 
+
+//create playGame function to run playRound 5 times
+//move playRound into playGame (declare playRound in playGame)
+// create for loop for 5 rounds
+// pass the choice functions as parameters into playRound
+
+//console log the score
+// console log the win/lose message using conditional (if else) statement
 
 
 
-//store computer choice in a variable
-// const computerFinalAnswer = getComputerChoice()
+
+
+
 
 let humanScore = 0
 let computerScore = 0
@@ -23,8 +30,7 @@ playGame()
 
 //create function to generate computer choice
 function getComputerChoice() {
-    let compChoice = Math.random() * 100
-    console.log(compChoice)    
+    let compChoice = Math.random() * 100  
     if (compChoice <= 33.333){
    return "rock" 
 }
@@ -37,19 +43,22 @@ else {
 }
 }
 
-
-// get choice from user
+ // get choice from user
+// create function for human choice from prompt and convert to lowercase string 
 function getHumanChoice() {
-    let humanChoice = prompt("Type your choice: rock, paper or scissors")
+   let humanChoice = prompt("Type your choice: rock, paper or scissors")
 return humanChoice.toLowerCase()
 }
     
 
+//create playGame function to run playRound 5 times
+//move playRound into playGame (declare playRound in playGame)
+// create for loop for 5 rounds
+// pass the choice functions as parameters into playRound
+
 
 function playGame(){
     for (i = 0; i < 5; i++){
-        // getComputerChoice();
-        // getHumanChoice();
         playRound(getHumanChoice(),getComputerChoice());
     }
     function playRound (getHumanChoice, getComputerChoice){
@@ -89,6 +98,10 @@ function playGame(){
      
 }
 
+
+//console log the score
+// console log the win/lose message using conditional (if else) statement
+
 console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`)
 
 if (humanScore === computerScore){
@@ -100,8 +113,13 @@ else if(humanScore > computerScore){
 else {
     console.log("You LOSE!!!")
 }
-// console.log(`You chose: ${getHumanChoice}, Computer chose: ${getComputerChoice}`)
 
+
+
+//create playRound function 
+// compare choices in playRound using logical operators (&&)
+//use conditional statements (if, else) to return score
+//console log the results 
 
 // function playRound (humanFinalAnswer, computerFinalAnswer){
 //     if (humanFinalAnswer === computerFinalAnswer){
@@ -140,50 +158,6 @@ else {
 // }
 
 
-
-
-// function playGame(){
-//     for (i = 0; i < 5; i++){
-//         // getComputerChoice();
-//         // getHumanChoice();
-//         playRound(getHumanChoice(),getComputerChoice());
-//     }
-//     function playRound (getHumanChoice, getComputerChoice){
-//         if (getHumanChoice === getComputerChoice){
-//             console.log("Draw") 
-//         }
-    
-//         else if (getHumanChoice === "rock" && getComputerChoice === "scissors"){
-//             console.log("You win! Rock beats scissors");
-//             return humanScore++;
-//         }
-//         else if (getHumanChoice === "rock" && getComputerChoice === "paper") {
-//             console.log("You Lose! Paper beats rock")
-//             return computerScore++;
-//         }
-//         else if (getHumanChoice === "paper" && getComputerChoice === "rock"){
-//             console.log("You Win! Paper beats rock")
-//             return humanScore++;
-//         }
-    
-//         else if (getHumanChoice === "paper" && getComputerChoice === "scissors"){
-//             console.log("You Lose! Scissors beat paper")
-//             return computerScore++;
-//         }
-    
-//         else if (getHumanChoice === "scissors" && getComputerChoice === "paper"){
-//             console.log("You Win! Scissors beat paper")
-//             return humanScore++;
-//         }
-    
-//         else {
-//             console.log("You Lose! Rock beats scissors")
-//             return computerScore++;
-//         }   
-    
-//     }
-     
-// }
 
 
 
